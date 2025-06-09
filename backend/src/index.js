21a056dcc0364c92ef7af22c5d913c826682e4bf
Michael Harrison <harrim7@gmail.com>
2025-06-09 13:55:55 -0600

    Add missing files for React build

---
+++ backend/src/index.js
@@ -0,0 +1,4 @@
+// This is a reference to the server.js file in the root of the backend directory
+// We're keeping this file to maintain compatibility with the existing package.json
+
+require('../server');
